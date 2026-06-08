2894f053185ef5eccec2429bf82a32d5c32b0074
Manuel Christlieb <manuel@christlieb.eu>
2026-06-08 22:31:34 +0200

    feat: add useDependentField hook

---
+++ resources/js/form/components/use-dependent-field.ts
@@ -0,0 +1,16 @@
+import { getBooleanProp } from "@lattice/core/props";
+import type { Node } from "@lattice/core/types";
+import { type FieldConditions, type FieldState, evaluateConditions } from "./conditions";
+import { useFormValues } from "./values";
+
+export function useDependentField(node: Node): FieldState {
+  const values = useFormValues();
+  const conditions = node.props?.conditions as FieldConditions | undefined;
+
+  return evaluateConditions(conditions, values, {
+    hidden: getBooleanProp(node.props, "hidden"),
+    required: getBooleanProp(node.props, "required"),
+    readonly: getBooleanProp(node.props, "readonly"),
+    disabled: getBooleanProp(node.props, "disabled"),
+  });
+}
