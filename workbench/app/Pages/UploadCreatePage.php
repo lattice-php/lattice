9d422f5754685302ebb41fc43cd04069cda04879
Manuel Christlieb <manuel@christlieb.eu>
2026-06-14 00:20:05 +0200

    chore: add workbench uploads page for FileUpload

---
+++ workbench/app/Pages/UploadCreatePage.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Workbench\App\Pages;
+
+use Lattice\Lattice\Attributes\Page;
+use Lattice\Lattice\Core\Components\Heading;
+use Lattice\Lattice\Core\Components\Stack;
+use Lattice\Lattice\Core\Enums\Gap;
+use Lattice\Lattice\Core\Enums\HttpMethod;
+use Lattice\Lattice\Core\PageSchema;
+use Lattice\Lattice\Forms\Components\Form;
+use Workbench\App\Forms\UploadForm;
+
+#[Page(route: '/uploads/create')]
+class UploadCreatePage extends WorkbenchPage
+{
+    public function title(): string
+    {
+        return __('workbench.pages.upload-create.title');
+    }
+
+    public function render(PageSchema $schema): PageSchema
+    {
+        return $schema->schema([
+            Stack::make('upload-create-page')
+                ->gap(Gap::Large)
+                ->schema([
+                    Heading::make(__('workbench.pages.upload-create.heading')),
+                    Form::use(UploadForm::class)
+                        ->method(HttpMethod::Post)
+                        ->submitLabel(__('workbench.pages.upload-create.submit')),
+                ]),
+        ]);
+    }
+}
