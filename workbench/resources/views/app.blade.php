06df5ce693ee980fba1820dab03c1d8bd8c22dc1
Manuel Christlieb <manuel@christlieb.eu>
2026-06-15 22:37:18 +0200

    chore: add react-scan dev profiling to the workbench
    Loads react-scan in the workbench when Vite runs hot, and adds the missing
    @viteReactRefresh directive before @vite so @vitejs/plugin-react can install
    its preamble (fixes the "can't detect preamble" dev error).

--- workbench/resources/views/app.blade.php
+++ workbench/resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>Lattice</title>
+    @if (app()->isLocal() && ! app()->runningUnitTests() && Illuminate\Support\Facades\Vite::isRunningHot())
+        <script crossorigin="anonymous" src="https://unpkg.com/react-scan@0.5.7/dist/auto.global.js"></script>
+    @endif
+    @viteReactRefresh
     @vite(['workbench/resources/css/app.css', 'workbench/resources/js/app.tsx'])
     @inertiaHead
 </head>
