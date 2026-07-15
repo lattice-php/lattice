72a2a1998665e6caf1f3731d2e3f62f30fa5e823
Manuel Christlieb <manuel@christlieb.eu>
2026-07-15 20:57:38 +0200

    refactor(ui)!: chart series colors take Color values

--- src/Ui/Values/ChartSeries.php
+++ src/Ui/Values/ChartSeries.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace Lattice\Lattice\Ui\Values;
 
 use Lattice\Lattice\Attributes\TypeScript;
+use Lattice\Lattice\Core\Color;
+use Lattice\Lattice\Core\Enums\ColorName;
 use Lattice\Lattice\Ui\Enums\ChartSeriesType;
 
 #[TypeScript]
@@ -13,45 +15,50 @@ final readonly class ChartSeries
         public ChartSeriesType $type,
         public string $dataKey,
         public string $name,
-        public ?string $color = null,
+        public ?Color $color = null,
         public ?string $stackId = null,
         public ?string $nameKey = null,
         public string $innerRadius = '0%',
         public ?float $maxValue = null,
     ) {}
 
-    public static function line(string $dataKey, ?string $name = null, ?string $color = null): self
+    public static function line(string $dataKey, ?string $name = null, Color|ColorName|string|null $color = null): self
     {
-        return new self(ChartSeriesType::Line, $dataKey, $name ?? $dataKey, $color);
+        return new self(ChartSeriesType::Line, $dataKey, $name ?? $dataKey, self::resolveColor($color));
     }
 
-    public static function bar(string $dataKey, ?string $name = null, ?string $color = null, ?string $stackId = null): self
+    public static function bar(string $dataKey, ?string $name = null, Color|ColorName|string|null $color = null, ?string $stackId = null): self
     {
-        return new self(ChartSeriesType::Bar, $dataKey, $name ?? $dataKey, $color, $stackId);
+        return new self(ChartSeriesType::Bar, $dataKey, $name ?? $dataKey, self::resolveColor($color), $stackId);
     }
 
-    public static function area(string $dataKey, ?string $name = null, ?string $color = null, ?string $stackId = null): self
+    public static function area(string $dataKey, ?string $name = null, Color|ColorName|string|null $color = null, ?string $stackId = null): self
     {
-        return new self(ChartSeriesType::Area, $dataKey, $name ?? $dataKey, $color, $stackId);
+        return new self(ChartSeriesType::Area, $dataKey, $name ?? $dataKey, self::resolveColor($color), $stackId);
     }
 
-    public static function pie(string $dataKey, ?string $nameKey = null, ?string $name = null, ?string $color = null): self
+    public static function pie(string $dataKey, ?string $nameKey = null, ?string $name = null, Color|ColorName|string|null $color = null): self
     {
-        return new self(ChartSeriesType::Pie, $dataKey, $name ?? $dataKey, $color, nameKey: $nameKey);
+        return new self(ChartSeriesType::Pie, $dataKey, $name ?? $dataKey, self::resolveColor($color), nameKey: $nameKey);
     }
 
-    public static function doughnut(string $dataKey, ?string $nameKey = null, ?string $name = null, ?string $color = null, string $innerRadius = '60%'): self
+    public static function doughnut(string $dataKey, ?string $nameKey = null, ?string $name = null, Color|ColorName|string|null $color = null, string $innerRadius = '60%'): self
     {
-        return new self(ChartSeriesType::Pie, $dataKey, $name ?? $dataKey, $color, nameKey: $nameKey, innerRadius: $innerRadius);
+        return new self(ChartSeriesType::Pie, $dataKey, $name ?? $dataKey, self::resolveColor($color), nameKey: $nameKey, innerRadius: $innerRadius);
     }
 
-    public static function gauge(string $dataKey, ?string $nameKey = null, ?string $name = null, ?string $color = null, ?float $maxValue = null, string $innerRadius = '70%'): self
+    public static function gauge(string $dataKey, ?string $nameKey = null, ?string $name = null, Color|ColorName|string|null $color = null, ?float $maxValue = null, string $innerRadius = '70%'): self
     {
-        return new self(ChartSeriesType::Gauge, $dataKey, $name ?? $dataKey, $color, nameKey: $nameKey, innerRadius: $innerRadius, maxValue: $maxValue);
+        return new self(ChartSeriesType::Gauge, $dataKey, $name ?? $dataKey, self::resolveColor($color), nameKey: $nameKey, innerRadius: $innerRadius, maxValue: $maxValue);
     }
 
-    public static function distribution(string $dataKey, ?string $nameKey = null, ?string $name = null, ?string $color = null): self
+    public static function distribution(string $dataKey, ?string $nameKey = null, ?string $name = null, Color|ColorName|string|null $color = null): self
     {
-        return new self(ChartSeriesType::Distribution, $dataKey, $name ?? $dataKey, $color, nameKey: $nameKey);
+        return new self(ChartSeriesType::Distribution, $dataKey, $name ?? $dataKey, self::resolveColor($color), nameKey: $nameKey);
+    }
+
+    private static function resolveColor(Color|ColorName|string|null $color): ?Color
+    {
+        return $color === null ? null : Color::from($color);
     }
 }
