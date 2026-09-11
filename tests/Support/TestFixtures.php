3982250d1256c18890e032734798634ebffcb60f
Manuel Christlieb <manuel@christlieb.eu>
2026-09-11 17:44:07 +0200

    chore: let phpstan read the option-source fixture's constructor type
    The inline @var on the promoted parameter shadowed the constructor's
    @param for phpstan, which then reported the array as untyped.

--- tests/Support/TestFixtures.php
+++ tests/Support/TestFixtures.php
@@ -101,7 +101,6 @@ function inMemoryOptionSource(array $people): OptionSource
          * @param  array<int|string, string>  $people
          */
         public function __construct(
-            /** @var array<int|string, string> */
             private array $people,
         ) {}
 
